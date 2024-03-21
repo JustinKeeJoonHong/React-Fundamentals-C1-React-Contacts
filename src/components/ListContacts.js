e2509c2b4af680f743fcac05eec3294100f14c07
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-21 11:26:46 -0700

    give props to ListContacts function

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,5 +1,6 @@
-const ListContacts = () => {
-  return <ol></ol>;
+const ListContacts = ({ contacts }) => {
+  console.log(contacts);
+  return <ol className="contact-list"></ol>;
 };
 
 export default ListContacts;
