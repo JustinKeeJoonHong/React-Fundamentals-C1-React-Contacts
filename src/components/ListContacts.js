d63923292f170c83004dcac0efe58bbe6745522d
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-28 13:27:10 -0700

    add message showing number of current contacts

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -26,6 +26,14 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
           onChange={(event) => updateQuery(event.target.value)}
         />
       </div>
+
+      {showingContacts.length !== contacts.length && (
+        <div className="showing-contacts">
+          <span>
+            Now showing {showingContacts.length} of {contacts.length}
+          </span>
+        </div>
+      )}
       <ol className="contact-list">
         {showingContacts.map((contact) => (
           <li key={contact.id} className="contact-list-item">
